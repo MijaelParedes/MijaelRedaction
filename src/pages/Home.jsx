8588919cf89986c5e8d9b81820b9ef5e545c1925
MijaelParedes <60280249+MijaelParedes@users.noreply.github.com>
2026-04-29 00:44:26 -0500

    push ultimo mascota

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import Mascota from "../../components/Mascota";
+import Mascota from "../../components/mascota";
 import MascotaFlotante from "/components/MascotaFlotante";
 
 export default function Home() {
